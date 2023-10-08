dfce470a35acb28f0fe884dbf7b1e7b266afe3b2
Vincent Collard <vincoll@outlook.com>
2023-10-08 22:50:13 +0200

    refact: Dagger docker with func

--- ci/main.go
+++ ci/main.go
@@ -35,13 +35,34 @@ func main() {
 		return
 	}
 	defer client.Close()
-	////////////////////////////////////////////////////////////
 
-	// get host directory
-	project := client.Host().Directory(".")
+	// Build Docker Multistage ---
+	ctnrs, err := buildImage(ctx, client)
+	if err != nil {
+		panic(fmt.Errorf("failed to build docker image: %w", err))
+	}
+
+	for _, ctnr := range ctnrs {
+
+		tags := []string{Help.ShaShort, "latest"}
+
+		err := publishImage(ctx, client, ctnr, tags)
+		if err != nil {
+			panic(fmt.Errorf("failed to publish image: %w", err))
+		}
+
+	}
+	fmt.Println("Done")
+}
+
+// buildImage builds a docker image with a multistage build
+func buildImage(ctx context.Context, client *dagger.Client) ([]*dagger.Container, error) {
 
 	fmt.Println("Docker multistage build...")
-	// build app
+	ctnrs := []*dagger.Container{}
+
+	project := client.Host().Directory(".")
+
 	builderStage := client.Container().
 		From(fmt.Sprintf("golang:%s-alpine", "1.21")).
 		WithEnvVariable("CGO_ENABLED", "0").
@@ -63,9 +84,8 @@ func main() {
 		// include a cache for go build
 		WithMountedCache("/root/.cache/go-build", client.CacheVolume("go-build"))
 
-	// publish binary on alpine base
 	finalStage := client.Container().
-		From("alpine").
+		From("alpine:latest").
 		WithLabel("org.opencontainers.image.title", "vigie").
 		WithLabel("org.opencontainers.image.version", Help.ShaShort).
 		WithLabel("org.opencontainers.image.created", Help.DateRFC3339).
@@ -74,29 +94,23 @@ func main() {
 		WithEntrypoint([]string{"/vigie"}).
 		WithDefaultArgs(dagger.ContainerWithDefaultArgsOpts{Args: []string{"version"}})
 
-	// Publish to Registry
+	ctnrs = append(ctnrs, finalStage)
 
-	finalStage = finalStage.WithRegistryAuth("ghcr.io", "vincoll", client.SetSecret("gh_token", Sec.GITHUB_TOKEN))
-
-	tags := []string{Help.ShaShort, "latest"}
-	for _, tag := range tags {
-
-		addr, err := finalStage.Publish(ctx, fmt.Sprintf("ghcr.io/%s/vigie:%s", "vincoll", tag))
-		if err != nil {
-			panic(fmt.Errorf("failed to publish image: %w", err))
-		}
-		fmt.Printf("Published image to :%s\n", addr)
-	}
-
-	fmt.Println(finalStage)
-
+	return ctnrs, nil
 }
 
-func buildImage(ctx context.Context, client *dagger.Client) (error, []*dagger.Container) {
-	fmt.Println("Building with Dagger")
+func publishImage(ctx context.Context, client *dagger.Client, ctnr *dagger.Container, tags []string) error {
 
-	// Dagger https://github.com/dagger/dagger/issues/4567
-	return nil, nil
+	// Publish to Registry ---
+	ctnr = ctnr.WithRegistryAuth("ghcr.io", "vincoll", client.SetSecret("gh_token", Sec.GITHUB_TOKEN))
+	for _, tag := range tags {
+		addr, err := ctnr.Publish(ctx, fmt.Sprintf("ghcr.io/%s/vigie:%s", "vincoll", tag))
+		if err != nil {
+			return err
+		}
+		fmt.Printf("published image to :%s\n", addr)
+	}
+	return nil
 }
 
 var Help Helpers
@@ -125,11 +139,11 @@ func getSHA() (shortSha string, longSha string) {
 	return sha[0:7], sha
 }
 
-//
 /*
 
 https://github.com/dagger/dagger/blob/25be91c8ea851e356563727c5a4a8c69d82f6399/internal/mage/util/util.go#L118
 https://github.com/flipt-io/flipt/blob/dd47bb474870be7bb83f887a38f3b1875ebb9371/build/internal/flipt.go#L126
+	// Dagger https://github.com/dagger/dagger/issues/4567
 
 
 */
